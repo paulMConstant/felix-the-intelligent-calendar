590f8e1182180ccea9731da14dd01e0855037ac9
Paul Constant <constantpaul@hotmail.fr>
2021-03-15 14:06:57 +0100

    Add cost function for insertions (TODO optimize it)

--- felix-backend/felix-computation-api/src/compute_insertion_costs.rs
+++ felix-backend/felix-computation-api/src/compute_insertion_costs.rs
@@ -16,11 +16,12 @@ pub fn compute_insertion_costs(
     let mut scores_for_all_activities = Vec::with_capacity(static_data.len());
 
     for activity_static_data in static_data {
-        let mut possible_beginnings = Vec::with_capacity(
-            activity_static_data
-                .possible_insertion_beginnings_minutes_sorted
-                .len(),
-        );
+        let mut possible_beginnings = Vec::new();
+        // TODO check vs Vec::with_capacity(
+            //activity_static_data
+                //.possible_insertion_beginnings_minutes_sorted
+                //.len(),
+        //);
 
         // 1 - Fetch incompatible beginnings
 
@@ -65,16 +66,50 @@ pub fn compute_insertion_costs(
                 .collect();
         }
 
-        // 2 - Calculate scores for the remaining activities
+        // 2 - Calculate scores for the remaining beginnings
         for beginning in activity_static_data
             .possible_insertion_beginnings_minutes_sorted
             .iter()
             .copied()
             .filter(|beginning| !incompatible_beginnings.contains(beginning))
         {
+            let end = beginning + activity_static_data.duration_minutes;
+
+            // The cost is the number of possible beginnings removed from other activities
+            let mut cost = 0;
+            for incompatible_activities_static_data in activity_static_data
+                .indexes_of_incompatible_activities
+                .iter()
+                .copied()
+                .filter_map(|index| unsafe {
+                    if insertion_data.get_unchecked(index).is_some() {
+                        // The activity is inserted, don't count it ?
+                        // TODO maybe count it as well ?
+                        None
+                    } else {
+                        Some(static_data.get_unchecked(index))
+                    }
+                }) {
+                    let offset_check_before_activity = 
+                        incompatible_activities_static_data.duration_minutes
+                        - MIN_TIME_DISCRETIZATION_MINUTES;
+
+                    let beginning_with_duration_offset = if beginning < offset_check_before_activity {
+                        0
+                    } else {
+                        beginning - offset_check_before_activity
+                    };
+                    let mut range = incompatible_activities_static_data
+                        .possible_insertion_beginnings_minutes_sorted
+                        .range(beginning_with_duration_offset..end);
+
+                    while range.next().is_some() {
+                        cost += 1;
+                    }
+                }
             possible_beginnings.push(InsertionCosts {
                 beginning_minutes: beginning,
-                cost: 1, // TODO
+                cost,
             });
         }
         scores_for_all_activities.push(possible_beginnings);
--- felix-backend/felix-computation-api/tests/compute_insertion_costs.rs
+++ felix-backend/felix-computation-api/tests/compute_insertion_costs.rs
@@ -53,7 +53,9 @@ fn test_insertion_costs_simplest() {
             duration_minutes: 10,
         },
         ActivityComputationStaticData {
-            possible_insertion_beginnings_minutes_sorted: btreeset_from_slice(&[]), // We don't care
+            possible_insertion_beginnings_minutes_sorted: btreeset_from_slice(&[
+                0, 5, 10, 20 
+            ]),
             indexes_of_incompatible_activities: vec![0],
             duration_minutes: 15,
         },
@@ -61,7 +63,29 @@ fn test_insertion_costs_simplest() {
 
     let insertion_data = vec![None, None];
 
-    let insertion_costs = compute_insertion_costs(&static_data, &insertion_data);
+    let insertion_costs = compute_insertion_costs(&static_data, &insertion_data)
+        .iter()
+        .map(|vec_insertion_cost| vec_insertion_cost
+             .iter()
+             .map(|insertion_cost| insertion_cost.cost)
+             .collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let insertion_costs0 = &insertion_costs[0];
+    assert_eq!(insertion_costs0[0], 2); // 0 Blocks 0, 5
+    assert_eq!(insertion_costs0[1], 3); // 5 Blocks 0, 5, 10
+    assert_eq!(insertion_costs0[2], 3); // 10 Blocks 0, 5, 10
+    assert_eq!(insertion_costs0[3], 3); // 15 Blocks 5, 10 20
+    assert_eq!(insertion_costs0[4], 2); // 20 Blocks 10, 20
+    assert_eq!(insertion_costs0[5], 0); // 35 Blocks nothing
+    assert_eq!(insertion_costs0[6], 0); // 45 Blocks nothing
+    assert_eq!(insertion_costs0[7], 0); // 50 Blocks nothing
+
+    let insertion_costs1 = &insertion_costs[1];
+    assert_eq!(insertion_costs1[0], 3); // 0 Blocks 0, 5, 10
+    assert_eq!(insertion_costs1[1], 4); // 5 Blocks 0, 5, 10, 15
+    assert_eq!(insertion_costs1[2], 4); // 10 Blocks 5, 10, 15, 20
+    assert_eq!(insertion_costs1[3], 2); // 15 Blocks 10, 20
 }
 
 fn btreeset_from_slice(slice: &[u16]) -> BTreeSet<u16> {
