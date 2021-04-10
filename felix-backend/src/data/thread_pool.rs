baff35dd85b227fddd7908bac25ec10ce44bc8b0
Paul Constant <constantpaul@hotmail.fr>
2021-04-10 10:28:00 +0200

    Add doc & use one more thread in FelixThreadPool

--- felix-backend/src/data/thread_pool.rs
+++ felix-backend/src/data/thread_pool.rs
@@ -1,3 +1,5 @@
+/// Simple wrapper around a rayon ThreadPool.
+/// Has juste one method and is initialized with the number of threads on the machine.
 #[derive(Debug)]
 pub(crate) struct FelixThreadPool {
     thread_pool: rayon::ThreadPool,
@@ -7,7 +9,7 @@ impl FelixThreadPool {
     pub fn new() -> Self {
         FelixThreadPool {
             thread_pool: rayon::ThreadPoolBuilder::new()
-                .num_threads((num_cpus::get() - 1).max(1))
+                .num_threads(num_cpus::get())
                 .build()
                 .expect("Could not initialize rayon ThreadPool"),
         }
