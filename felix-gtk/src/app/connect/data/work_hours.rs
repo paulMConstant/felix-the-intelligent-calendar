f5cee94708189fa91f070ce53c5690da50a6c393
Paul Constant <constantpaul@hotmail.fr>
2021-05-15 18:37:30 +0200

    Fix bug crash if open with no felix.json data

--- felix-gtk/src/app/connect/data/work_hours.rs
+++ felix-gtk/src/app/connect/data/work_hours.rs
@@ -10,7 +10,12 @@ impl App {
         events.connect_work_hours_changed(Box::new(clone!(@strong self.ui as ui => move |data| {
             let mut ui = ui.borrow_mut();
             ui.on_work_hours_changed(data);
-            ui.on_custom_work_hours_changed(data);
+
+            // No need to update custom work hours if no entity is selected
+            if ui.current_entity().is_some() {
+                ui.on_custom_work_hours_changed(data);
+            }
+
             ui.update_schedules(data);
         })));
     }
