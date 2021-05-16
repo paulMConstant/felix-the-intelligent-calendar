e9c1902b500696cf80ed6bc10fbb86e2e6e76155
Paul Constant <constantpaul@hotmail.fr>
2021-05-16 14:34:04 +0200

    UI windows can now be moved & resized

--- felix-gtk/src/app/connect/ui/header.rs
+++ felix-gtk/src/app/connect/ui/header.rs
@@ -6,7 +6,6 @@ impl App {
     pub fn connect_header_buttons(&self) {
         fetch_from!(self.ui.borrow(), data_window, data_button);
 
-        data_window.resize(800, 600);
         data_window.connect_delete_event(move |window, _| {
             window.hide();
             glib::signal::Inhibit(true)
