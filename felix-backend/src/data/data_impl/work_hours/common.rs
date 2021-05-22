dffa320bd2c8d9fb0d4fbff69337fb21271ff90d
Paul Constant <constantpaul@hotmail.fr>
2021-05-22 10:45:05 +0200

    Move custom work hours functions from entities to work_hours impl

--- felix-backend/src/data/data_impl/work_hours/common.rs
+++ felix-backend/src/data/data_impl/work_hours/common.rs
@@ -1,6 +1,6 @@
 //! Functions used by both global and custom work hours.
 
-use crate::data::Data;
+use crate::data::{Time, Data};
 use crate::errors::{
     change_work_hours_while_activity_inserted::ChangeWorkHoursWhileActivityInserted, Result,
 };
@@ -21,5 +21,35 @@ impl Data {
         } else {
             Ok(())
         }
+    }
+
+    /// Returns the time taken by the activities of an entity.
+    ///
+    /// If the entity does not exist, returns Time(0, 0).
+    #[must_use]
+    pub(super) fn time_taken_by_activities(&self, entity_name: &str) -> Time {
+        self.activities_sorted()
+            .iter()
+            .filter_map(|activity| {
+                if activity.entities_sorted().contains(&entity_name.into()) {
+                    Some(activity.duration())
+                } else {
+                    None
+                }
+            })
+            .sum()
+    }
+
+    /// Returns the total time available for an entity.
+    ///
+    /// # Errors
+    ///
+    /// Returns Err if the entity does not exist.
+    pub(super) fn total_available_time(&self, entity_name: &str) -> Result<Time> {
+        Ok(self
+            .work_hours_of(entity_name)?
+            .iter()
+            .map(|interval| interval.duration())
+            .sum())
     }
 }
--- felix-backend/src/data/data_impl/work_hours/error_checks.rs
+++ felix-backend/src/data/data_impl/work_hours/error_checks.rs
@@ -1,7 +1,7 @@
 //! Helper functions for work_hours implementation of data.
 
-use crate::data::{Data, Time};
-use crate::errors::{not_enough_time::NotEnoughTime, Result};
+use crate::data::{Data, Time, TimeInterval};
+use crate::errors::{does_not_exist::DoesNotExist, not_enough_time::NotEnoughTime, Result};
 
 impl Data {
     /// Checks if an entity has not enough time to update a work interval.
@@ -54,5 +54,116 @@ impl Data {
                     .expect("Could not get entity listed in data.entities_sorted()")
                     < required_free_time
             })
+    }
+
+    /// Checks if the entity will have enough time for their activities
+    /// after adding a custom work hour.
+    ///
+    /// # Errors
+    ///
+    /// Returns Err if the entity does not exist or if it will not have enough time.
+    pub(super) fn check_entity_will_have_enough_time_with_custom_interval(
+        &self,
+        entity_name: &str,
+        interval_duration: Time,
+    ) -> Result<()> {
+        if self
+            .entity(entity_name)? // Check if entity exists here
+            .custom_work_hours()
+            .is_empty()
+        {
+            let activity_duration = self.time_taken_by_activities(&entity_name);
+            if interval_duration < activity_duration {
+                return Err(NotEnoughTime::work_hours_shortened_for(entity_name));
+            }
+        }
+        Ok(())
+    }
+
+    /// Checks if the entity has a custom work interval corresponding to the given one.
+    ///
+    /// # Errors
+    ///
+    /// Returns Err if the entity does not have a custom work interval corresponding to the given
+    /// one.
+    pub(super) fn check_entity_has_custom_interval(
+        &self,
+        entity_name: &str,
+        interval: &TimeInterval,
+    ) -> Result<()> {
+        // First, check if the entity has a corresponding custom work interval
+        if self
+            .entity(entity_name)?
+            .custom_work_hours()
+            .contains(interval)
+        {
+            Ok(())
+        } else {
+            Err(DoesNotExist::interval_does_not_exist(*interval))
+        }
+    }
+
+    /// Checks that the entity will still have enough free time after deleting a custom work
+    /// interval.
+    ///
+    /// # Errors
+    ///
+    /// Returns Err if the entity name is empty, the entity is not found or the entity will not
+    /// have enough free time.
+    ///
+    /// # Panics
+    ///
+    /// Panics if interval\_duration is greater than the custom work hours's total duration.
+    pub(super) fn check_entity_will_have_enough_time_after_deletion_of_interval(
+        &self,
+        entity_name: &str,
+        interval_duration: Time,
+    ) -> Result<()> {
+        // Check if the entity has enough free time
+        let custom_work_hours = self.entity(entity_name)?.custom_work_hours();
+        let entity_time = if custom_work_hours.len() == 1 {
+            // This is the last custom work hours.
+            // We should check that the global work hours will suffice.
+            self.work_hours()
+                .iter()
+                .map(|interval| interval.duration())
+                .sum()
+        } else {
+            // We should check that the remaining custom work hours will suffice.
+            let time_before_deletion: Time = custom_work_hours
+                .iter()
+                .map(|interval| interval.duration())
+                .sum();
+            time_before_deletion - interval_duration
+        };
+        if entity_time < self.time_taken_by_activities(entity_name) {
+            Err(NotEnoughTime::work_hours_shortened_for(entity_name))
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Checks if the entity will have enough time after time interval update.
+    ///
+    /// # Errors
+    ///
+    /// Returns Err if the entity name is empty, the entity is not found
+    /// or the entity will not have enough time after update.
+    pub(super) fn check_entity_will_have_enough_time_after_update(
+        &self,
+        entity_name: &str,
+        old_duration: Time,
+        new_duration: Time,
+    ) -> Result<()> {
+        if new_duration >= old_duration {
+            Ok(())
+        } else {
+            let required_free_time = old_duration - new_duration;
+            if self.free_time_of(entity_name)? < required_free_time {
+                Err(NotEnoughTime::work_hours_shortened_for(entity_name))
+            } else {
+                Ok(())
+            }
+        }
     }
 }
