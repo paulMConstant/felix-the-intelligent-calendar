fde16d293f62c69a5420ce49a12545693aaa940b
Paul Constant <constantpaul@hotmail.fr>
2021-06-22 19:53:31 +0200

    Update version string

--- felix-gtk/src/config.rs
+++ felix-gtk/src/config.rs
@@ -3,4 +3,4 @@ pub const APP_NAME: &str = "Felix";
 
 pub const DATA_CONF_FILE: &str = "./felix.json";
 pub const UI_STATE_FILE: &str = "./felix-ui-state.json";
-pub const VERSION: &str = "0.1.0";
+pub const VERSION: &str = "0.1.1";
